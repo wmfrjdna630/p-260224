33c3d2afc960dd959c75eb149a8ff8f93133bef2
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-24 15:14:56 +0900

    시작

---
+++ src/main/java/com/back/JsbExamApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsbExamApplication.class, args);
+	}
+
+}
