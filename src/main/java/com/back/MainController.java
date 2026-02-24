b9521003b4036ac8460a16f7d491b9b196b19209
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-02-24 15:39:27 +0900

    @ResponseBody

--- src/main/java/com/back/MainController.java
+++ src/main/java/com/back/MainController.java
@@ -2,17 +2,20 @@ package com.back;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/index")
-    public void index(){
-        System.out.println("index");
+    @ResponseBody
+    public String index(){
+        return "index";
     }
 
     @GetMapping("/hello")
-    public void hello(){
-        System.out.println("hello");
+    @ResponseBody
+    public String hello(){
+        return "hello";
     }
 }
