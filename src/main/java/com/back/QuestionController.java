f69aef61582e1ee512dac2540d8d364c16eeec40
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2026-03-03 12:51:52 +0900

    htm타ì임리프 해보기

--- src/main/java/com/back/QuestionController.java
+++ src/main/java/com/back/QuestionController.java
@@ -3,7 +3,10 @@ package com.back;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,7 +15,12 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<Question> questionList = questionRepository.findAll();
+
+        model.addAttribute("myAge", 20);
+        model.addAttribute("questionList", questionList);
 
         return "question_list";
     }
